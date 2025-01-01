214db8ed759db8ee0ac5c863460866b000300bb2
pauloocm <paulooo.cm@gmail.com>
2025-01-01 10:45:18 -0300

    Implemented the update method

--- TaskManager.Platform/Application/ITaskAppService.cs
+++ TaskManager.Platform/Application/ITaskAppService.cs
@@ -5,5 +5,6 @@
         Task<Guid> Add(AddTaskCommand command, CancellationToken ct = default);
         Task Complete(CompleteTaskCommand command, CancellationToken ct = default);
         Task<List<TaskDto>> Search(SearchTasksFilter filter, CancellationToken ct = default);
+        Task Update(UpdateTaskCommand command, CancellationToken ct = default);
     }
 }
