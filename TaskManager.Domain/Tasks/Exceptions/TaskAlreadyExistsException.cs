0da8bce088174e4fa4099628cef4e88abd81ba16
pauloocm <paulooo.cm@gmail.com>
2025-02-08 07:29:28 -0300

    Added name validation and refactored the unit tests

---
+++ TaskManager.Domain/Tasks/Exceptions/TaskAlreadyExistsException.cs
@@ -0,0 +1,6 @@
+﻿namespace TaskManager.Domain.Tasks.Exceptions
+{
+    public class TaskAlreadyExistsException(string title) : Exception($"Task with the specified title {title} already exists")
+    {
+    }
+}
